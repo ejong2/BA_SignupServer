7c156011abd837efe0c9cd7d7819995476e67a5a
enamu <guyus5594@gmail.com>
2022-12-05 17:34:02 +0900

    회원가입 서버 코드 정리

--- Project_UE_JoinServerSource/main.cpp
+++ Project_UE_JoinServerSource/main.cpp
@@ -16,6 +16,8 @@
 #pragma comment(lib, "WS2_32.lib")
 #pragma comment (lib, "mysqlcppconn.lib")
 
+#define PORT 19935
+#define IP_ADDRESS "127.0.0.1"
 #define PACKET_SIZE 50
 
 using namespace std;
@@ -28,14 +30,12 @@ vector<SOCKET> vSocketList;
 
 CRITICAL_SECTION ServerCS;
 
-// sql 세팅
 sql::Driver* driver = nullptr;
 sql::Connection* con = nullptr;
 sql::Statement* stmt = nullptr;
 sql::PreparedStatement* pstmt = nullptr;
 sql::ResultSet* rs = nullptr;
 
-// MultyByte -> UTF8 한글 변환
 std::string MultiByteToUtf8(std::string multibyte_str)
 {
     char* pszIn = new char[multibyte_str.length() + 1];
@@ -85,10 +85,10 @@ unsigned WINAPI WorkThread(void* Args)
     while (true)
     {
 
-        char Buffer[PACKET_SIZE] = { 0, };
-        char Buffer2[PACKET_SIZE] = { 0, };
+        char IdBuffer[PACKET_SIZE] = { 0, };
+        char PwdBuffer[PACKET_SIZE] = { 0, };
 
-        int RecvBytes = recv(CS, Buffer, sizeof(Buffer), 0);
+        int RecvBytes = recv(CS, IdBuffer, sizeof(IdBuffer), 0);
         if (RecvBytes <= 0)
         {
             cout << "클라이언트 연결 종료 : " << CS << '\n';
@@ -99,10 +99,10 @@ unsigned WINAPI WorkThread(void* Args)
             LeaveCriticalSection(&ServerCS);
             break;
         }
-        Buffer[PACKET_SIZE - 1] = '\0';
-        string strID = Buffer;
+        IdBuffer[PACKET_SIZE - 1] = '\0';
+        string strID = IdBuffer;
 
-        RecvBytes = recv(CS, Buffer2, sizeof(Buffer2), 0);
+        RecvBytes = recv(CS, PwdBuffer, sizeof(PwdBuffer), 0);
         if (RecvBytes <= 0)
         {
             cout << "클라이언트 연결 종료 : " << CS << '\n';
@@ -113,10 +113,10 @@ unsigned WINAPI WorkThread(void* Args)
             LeaveCriticalSection(&ServerCS);
             break;
         }
-        Buffer2[PACKET_SIZE - 1] = '\0';
-        string strPWD = Buffer2;
+        PwdBuffer[PACKET_SIZE - 1] = '\0';
+        string strPWD = PwdBuffer;
 
-        pstmt = con->prepareStatement("INSERT INTO GMUSER(`USER_ID`,`USER_PWD`) VALUES(?, ?)");
+        pstmt = con->prepareStatement("INSERT INTO UserTable(`ID`,`PWD`) VALUES(?, ?)");
         pstmt->setString(1, strID);
         pstmt->setString(2, strPWD);
         pstmt->execute();
@@ -129,9 +129,9 @@ unsigned WINAPI WorkThread(void* Args)
             int TotalSentBytes = 0;
             do
             {
-                SendBytes = send(vSocketList[i], &Buffer2[TotalSentBytes], sizeof(Buffer2) - TotalSentBytes, 0);
+                SendBytes = send(vSocketList[i], &PwdBuffer[TotalSentBytes], sizeof(PwdBuffer) - TotalSentBytes, 0);
                 TotalSentBytes += SendBytes;
-            } while (TotalSentBytes < sizeof(Buffer2));
+            } while (TotalSentBytes < sizeof(PwdBuffer));
 
             if (SendBytes <= 0)
             {
@@ -149,43 +149,35 @@ unsigned WINAPI WorkThread(void* Args)
 
 int main()
 {
-    // DB 연결
     driver = get_driver_instance();
     con = driver->connect(server, username, password);
     con->setSchema("LoginSheet");
-    cout << "데이터베이스 접속성공!" << endl;
 
-    //// 테이블 생성
-    //stmt = con->createStatement();
-    //stmt->execute("DROP TABLE IF EXISTS BOARD");
-    //cout << "Finished dropping table (if existed)" << endl;
-    //stmt->execute("CREATE TABLE BOARD(ID_BOARD serial PRIMARY KEY, CONTENTS VARCHAR(100);");
-    //cout << "Finished creating table" << endl;
-    //delete stmt;
+    cout << "[회원가입 서버 활성화]" << '\n';
 
     InitializeCriticalSection(&ServerCS);
 
     WSADATA WSAData;
     WSAStartup(MAKEWORD(2, 2), &WSAData);
-    SOCKET LS = socket(AF_INET, SOCK_STREAM, 0);
+    SOCKET SS = socket(AF_INET, SOCK_STREAM, 0);
 
-    SOCKADDR_IN LA = { 0, };
-    LA.sin_family = AF_INET;
-    LA.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
-    LA.sin_port = htons(19935);
+    SOCKADDR_IN SA = { 0, };
+    SA.sin_family = AF_INET;
+    SA.sin_addr.S_un.S_addr = inet_addr(IP_ADDRESS);
+    SA.sin_port = htons(PORT);
 
-    if (::bind(LS, (SOCKADDR*)&LA, sizeof(LA)) != 0) { exit(-3); };
-    if (listen(LS, SOMAXCONN) == SOCKET_ERROR) { exit(-4); };
+    if (::bind(SS, (SOCKADDR*)&SA, sizeof(SA)) != 0) { exit(-3); };
+    if (listen(SS, SOMAXCONN) == SOCKET_ERROR) { exit(-4); };
 
-    cout << "BLOCKING...." << '\n';
+    cout << "클라이언트 연결을 기다리는 중입니다......." << '\n';
 
     while (true)
     {
         SOCKADDR_IN CA = { 0, };
         int sizeCA = sizeof(CA);
-        SOCKET CS = accept(LS, (SOCKADDR*)&CA, &sizeCA);
+        SOCKET CS = accept(SS, (SOCKADDR*)&CA, &sizeCA);
 
-        cout << "CONNECT : " << CS << '\n';
+        cout << "클라이언트 접속 : " << CS << '\n';
 
         EnterCriticalSection(&ServerCS);
         vSocketList.push_back(CS);
@@ -193,7 +185,7 @@ int main()
 
         HANDLE hThread = (HANDLE)_beginthreadex(0, 0, WorkThread, (void*)&CS, 0, 0);
     }
-    closesocket(LS);
+    closesocket(SS);
 
     WSACleanup();
 }
